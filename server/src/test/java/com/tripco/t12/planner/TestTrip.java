e6df33e19c6235e4fa46eec99b7970949edaeb2b
henryjb9 <josh.b.henry@gmail.com>
2018-02-15 20:28:27 -0700

    This accounts for input format and calculates decimal fortmats. Passes all testFormat() tests

--- server/src/test/java/com/tripco/t12/planner/TestTrip.java
+++ server/src/test/java/com/tripco/t12/planner/TestTrip.java
@@ -37,4 +37,23 @@ public class TestTrip {
     // Call the equals() method of the first object on the second object.
     assertEquals(expectedDistances, trip.distances);
   }
+
+  @Test
+  public void testFormat() {
+    //method returns
+    assertEquals(trip.decCoord("0"),00.000,0);
+    //Letter handling
+    assertEquals(trip.decCoord("0° 0' 0\" N"), 00.000,0);
+    assertEquals(trip.decCoord("0° 0' 0\" W"), 00.000,0);
+    assertEquals(trip.decCoord("0° 0' 0\" E"), 00.000,0);
+    assertEquals(trip.decCoord("0° 0' 0\" S"), 00.000,0);
+    //Number inside range
+    assertEquals(trip.decCoord("12° 32' 45\" N"), 12.546,0.001);
+    assertEquals(trip.decCoord("41° 41' 41\" E"), 41.695,0.001);
+    assertEquals(trip.decCoord("32° 01.383' S"), -32.023,0.001);
+    assertEquals(trip.decCoord("58.988° W"), -58.988,0.001);
+    assertEquals(trip.decCoord("-40.001"), -40.001,.001);
+    //Numbers outside range
+    assertEquals(trip.decCoord("100° 32' 45\" N"), 0,0.001);
+  }
 }
