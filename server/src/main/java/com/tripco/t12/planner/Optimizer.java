ca8c4a3be56c54f9981be668537a8c012e64e7bb
henryjb9 <henryjb9@rams.colostate.edu>
2018-03-20 19:10:13 -0600

    added checkOpt in trip.java

---
+++ server/src/main/java/com/tripco/t12/planner/Optimizer.java
@@ -0,0 +1,37 @@
+package com.tripco.t12.planner;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.tripco.t12.server.HTTP;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import spark.Request;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
+public class Optimizer {
+    public Trip trip;
+    public ArrayList<Place> finArray;
+    public int[] finDist;
+
+
+
+    public void nearNeighbor(){
+
+    }
+
+    public void twoOpt(){
+
+    }
+
+    public void threeOpt(){
+
+    }
+}
