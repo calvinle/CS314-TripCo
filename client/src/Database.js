ba1f1f15d0238de7b2ef46d6f573dafe68ee3f98
calvinle <calvin.md.le@gmail.com>
2018-03-20 19:57:46 -0600

    Search input added

--- client/src/Database.js
+++ client/src/Database.js
@@ -8,18 +8,24 @@ class Database extends Component{
 
   render() {
 
-    return(
+    return (
 
         <div id="database" className="card border-0 border-dark">
           <div className="card-header bg-primary text-light">
-          Search
-        </div>
+            Search
+          </div>
           <div className="card-body">
-          AND HIS NAME IS JOHN CENA
-        </div>
-        </div>
-    )
-  }
-}
 
-export default Database;
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="basic-addon1">Search</span>
+              </div>
+              <input type="text" className="form-control" placeholder="Denver International Airport"></input>
+            </div>
+          </div>
+        </div>
+  )
+  }
+  }
+
+  export default Database;
