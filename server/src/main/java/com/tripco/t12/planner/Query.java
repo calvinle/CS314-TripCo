1162cf0598fc801862611d86efd84183ba8d494b
Tom <tomcavey@cs.colostate.edu>
2018-03-23 09:46:54 -0600

    Update Query.java

--- server/src/main/java/com/tripco/t12/planner/Query.java
+++ server/src/main/java/com/tripco/t12/planner/Query.java
@@ -1,9 +1,8 @@
 package com.tripco.t12.planner;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import com.tripco.t12.planner.SqlConnect;
+        import java.util.ArrayList;
+        import java.util.Arrays;
+        import com.tripco.t12.planner.SqlConnect;
 
 public class Query
 {
@@ -17,7 +16,20 @@ public class Query
         ArrayList<Place> test = new ArrayList<Place>();
         test = SqlConnect.getQ(query);
         this.places = test;
-        System.out.println(test.toString());
+        //System.out.println(test.toString());
+    }
+
+
+    public static void main (String[] args)
+    {
+        Query query = new Query();
+        query.query = "SELECT * FROM airports WHERE TYPE = \"closed\" ";
+
+        ArrayList<Place> test = new ArrayList<Place>();
+        test = SqlConnect.getQ(query.query);
+
+        System.out.println(test);
 
     }
+
 }
