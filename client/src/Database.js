04e80ad4c90f132975b9c1e6cba8e79ac1607d57
Tom <tomcavey@cs.colostate.edu>
2018-03-22 21:34:28 -0600

    Update Database.js

--- client/src/Database.js
+++ client/src/Database.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import SearchTable from './SearchTable';
 
 class Database extends Component {
     constructor(props) {
@@ -12,8 +13,38 @@ class Database extends Component {
             }
         };
 
-        this.updateQuery = this.updateQuery.bind(this);
+        this.updateQ = this.updateQ.bind(this);
     }
+
+    updateQuery(tffi) {
+        console.log("updateTrip");
+        console.log(tffi);
+        this.setState({query: tffi});
+    }
+
+    fetchResponse(){
+        // need to get the request body from the trip in state object.
+        let requestBody = this.props.query;
+
+        console.log(requestBody);
+
+        return fetch('http://' + location.host +'/query', {
+            method:"POST",
+            body: JSON.stringify(requestBody)
+        });
+    }
+
+    async plan(){
+        try {
+            let serverResponse = await this.fetchResponse();
+            let tffi = await serverResponse.json();
+            console.log(tffi);
+            this.props.updateQuery(tffi);
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
 
     newQuery(arg) {
         //this.props.updateOptions(arg);
@@ -23,12 +54,12 @@ class Database extends Component {
             type: this.state.query.type,
             query: arg,
             places: this.state.query.places
-        })
+        });
+        console.log("testQuery: ", testQuery);
         this.setState({query: testQuery});
-      //@TODO: Send file elsewhere
     }
 
-    updateQuery(event) {
+    updateQ(event) {
         console.log(document.getElementById("query").value);
         this.newQuery(document.getElementById("query").value);
     }
@@ -43,11 +74,12 @@ class Database extends Component {
 
                 <div className="card-body">
                     <div className="input-group-prepend">
-                        <div onClick={this.updateQuery.bind(this)}>
+                        <div onClick={this.updateQ.bind(this)}>
                             <button className="btn btn-success " id="queryButton"  type="button">Search</button>
                         </div>
                         <input type="text" id="query" className="form-control" placeholder="Location"></input>
                     </div>
+                    <SearchTable query={this.props.query} />
                 </div>
             </div>
         )
