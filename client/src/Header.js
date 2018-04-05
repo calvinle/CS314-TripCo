02db1cd72a9f5199dc645394e8d8c539cca25aea
William Schoonover <wcschoonover@gmail.com>
2018-04-04 23:54:40 -0600

    Organized top navbar and added title entry and menu dropdown. Closes #272, #248, #273

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,19 +1,37 @@
 import React, {Component} from 'react';
+import {Navbar, NavDropdown, Nav, Image, FormGroup, FormControl, Button, Glyphicon} from 'react-bootstrap';
 
 /* Renders a text heading above the application with useful information.
  */
 class Header extends Component{
   constructor(props) {
     super(props);
+    this.state = {
+      trip: {
+        title: "",
+      }
+    }
   }
 
   render() {
     return(
-        <div id="header" className="jumbotron" style={{"background-color": "#1E4D2B", "color":"white", "vertical-align":"middle"}}>
-            <img src={"http://www.cs.colostate.edu/~cs314/images/CompSci-NS-CSU-1-Hrev.png"} width={"30%"}/>
-        </div>
+      <Navbar id="header" fixedTop>
+        <Navbar.Form pullLeft>
+          <FormGroup>
+            <FormControl type="text" placeholder="Enter Title..." />
+          </FormGroup>{' '}
+          <Button type="submit">
+            <Glyphicon glyph="floppy-disk" />
+          </Button>
+        </Navbar.Form> 
+        <Image src="http://www.cs.colostate.edu/~cs314/images/CompSci-NS-CSU-1-Hrev.png" width="30%" id="topLogo"/>
+        <Nav pullRight>
+          <NavDropdown eventKey = {3} title = "Menu" id="menu">
+          </NavDropdown>
+        </Nav>
+      </Navbar>
     )
-  }
+  };
 
 }
 
