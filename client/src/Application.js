28a27048d1f477a060d924548afdd0f582433a4d
henryjb9 <josh.b.henry@gmail.com>
2018-04-08 16:52:27 -0600

    adds docready so client (specifically Application.js) has server config response as this.state.config. Do not merge until client and server are integrated

--- client/src/Application.js
+++ client/src/Application.js
@@ -21,6 +21,14 @@ class Application extends Component {
                 places: [],
                 distances: [],
                 map: "<svg width=\"1920\" height=\"20\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:svg=\"http://www.w3.org/2000/svg\"><g></g></svg>"
+            },
+            config : {
+                type: "",
+                version: "",
+                filters: [],
+                maps: [],
+                optimization: "",
+                units: []
             }
         }
         this.updateTrip = this.updateTrip.bind(this);
@@ -30,12 +38,42 @@ class Application extends Component {
         this.updateUserDef = this.updateUserDef.bind(this);
         this.addPlace = this.addPlace.bind(this);
         this.reduceList = this.reduceList.bind(this);
+        this.updateConfig = this.updateConfig.bind(this);
+        this.config = this.config.bind(this);
+        this.fetchResponse = this.fetchResponse.bind(this);
+    }
+
+    componentDidMount(){
+        console.log("DID MOUNT");
+        this.config();
+        console.log(this.state.config);
+    }
+
+    fetchResponse(){
+        console.log("Fetching");
+        return fetch('http://' + location.host +'/config');
+    }
+
+    async config(){
+        try {
+            let serverResponse = await this.fetchResponse();
+            let tffi = await serverResponse.json();
+            console.log("RESPONSE",tffi);
+            this.updateConfig(tffi);
+        } catch(err) {
+            console.error(err);
+        }
     }
 
     updateTrip(tffi) {
         console.log("updateTrip");
         console.log(tffi);
         this.setState({trip: tffi});
+    }
+
+    updateConfig(tffi){
+        console.log("updateConfig");
+        this.setState({config: tffi});
     }
 
     reduceList(place){
@@ -124,21 +162,21 @@ class Application extends Component {
                         <div id="map"></div>
                         <Row className="show-grid">
                             <Col sm={12}>
-                                <Options options={this.state.trip.options} updateOptions={this.updateOptions} updateUserDef={this.updateUserDef} updateOptimization={this.updateOptimization} />
+                                <Options options={this.state.trip.options} config={this.state.config} updateOptions={this.updateOptions} updateUserDef={this.updateUserDef} updateOptimization={this.updateOptimization} />
                             </Col>
 
                             <Col sm={12}>
-                                <Dropdown trip={this.state.trip} updateStart={this.updateStart} reduceList={this.reduceList} />
+                                <Dropdown trip={this.state.trip} config={this.state.config} updateStart={this.updateStart} reduceList={this.reduceList} />
                             </Col>
 
                             <Col sm={12}>
-                                <Destinations trip={this.state.trip} updateTrip={this.updateTrip} />
+                                <Destinations trip={this.state.trip} config={this.state.config} updateTrip={this.updateTrip} />
                             </Col>
                             <Col sm={12}>
-                                <Database trip={this.state.trip} addPlace={this.addPlace} />
+                                <Database trip={this.state.trip} config={this.state.config} addPlace={this.addPlace} />
                             </Col>
                             <Col sm={12}>
-                                <Trip trip={this.state.trip} updateTrip={this.updateTrip} />
+                                <Trip trip={this.state.trip} config={this.state.config} updateTrip={this.updateTrip} />
                             </Col>
                         </Row>
                     </Col>
