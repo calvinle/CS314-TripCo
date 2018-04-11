46980455c36608750e1f7d185e264a7f923e16db
calvinle <calvin.md.le@gmail.com>
2018-04-11 17:59:16 -0600

    Adding tests

--- server/src/test/java/com/tripco/t12/planner/TestOptimizer.java
+++ server/src/test/java/com/tripco/t12/planner/TestOptimizer.java
@@ -12,11 +12,12 @@ import java.util.Collections;
 import static org.junit.Assert.*;
 @RunWith(JUnit4.class)
 public class TestOptimizer {
+
     Trip trip;
     Optimizer optimizer;
 
     @Before
-    public void initialize(){
+    public void initialize() {
         trip = new Trip();
         trip.places = new ArrayList<>();
         Place place = new Place();
@@ -25,12 +26,12 @@ public class TestOptimizer {
     }
 
     @Test
-    public void testTrue(){
-        assertTrue(true==true);
+    public void testTrue() {
+        assertTrue(true == true);
     }
 
     @Test
-    public void testN(){
+    public void testN() {
         Place place = new Place();
         Place place0 = new Place();
         optimizer.workingArray.add(place);
@@ -39,33 +40,21 @@ public class TestOptimizer {
     }
 
     @Test
-    public void testSum(){
+    public void testSum() {
         ArrayList<Integer> n = new ArrayList<Integer>();
         n.add(2);
         n.add(5);
         assertEquals(optimizer.distSum(n), 7, 0);
+        n.add(7);
+        assertEquals(optimizer.distSum(n), 14, 0);
     }
 
-//    @Test
-//    public void testo2S(){
-//        Place a = new Place();
-//        Place b = new Place();
-//        Place c = new Place();
-//        Place d = new Place();
-//        Place e = new Place();
-//        Place f = new Place();
-//        Place g = new Place();
-//        Place h = new Place();
-//        ArrayList<Place> w = new ArrayList<Place>();
-//        w.add(a);
-//        w.add(b);
-//        w.add(c);
-//        w.add(d);
-//        w.add(e);
-//        w.add(f);
-//        w.add(g);
-//        w.add(h);
-//        w.add(a);
-//        optimizer.TwoOptSwap(4, 7);
-//    }
+    @Test
+    public void swapTest(){
+        int[] t = [0, 1, 2, 3, 4];
+        int i = 0;
+        int k = 3;
+        assertArrayEquals(optimizer.TwoOptSwap());
+    }
+
 }
