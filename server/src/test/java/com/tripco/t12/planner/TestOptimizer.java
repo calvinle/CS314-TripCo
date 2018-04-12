60bd5ac1757caeb3053cb134652b82db5880736d
calvinle <calvin.md.le@gmail.com>
2018-04-11 22:12:45 -0600

    Trip distance calculator (generate separate distances given a "list" of places)

--- server/src/test/java/com/tripco/t12/planner/TestOptimizer.java
+++ server/src/test/java/com/tripco/t12/planner/TestOptimizer.java
@@ -51,11 +51,8 @@ public class TestOptimizer {
 
     @Test
     public void swapTest(){
-
-
         int i = 0;
         int k = 3;
-        Place[] input = optimizer.TwoOptSwap(i, k);
         Place place0 = new Place();
         place0.id = "0";
         Place place1 = new Place();
@@ -66,8 +63,28 @@ public class TestOptimizer {
         place0.id = "3";
         Place place4 = new Place();
         place0.id = "0";
-        Place[] expected = {place0, place3, place2, place1, place0};
+        optimizer.tempArray.add(place0);
+        optimizer.tempArray.add(place1);
+        optimizer.tempArray.add(place2);
+        optimizer.tempArray.add(place3);
+        optimizer.tempArray.add(place4);
+        Place[] expected = {place3, place2, place1, place0, place4};
         assertArrayEquals(optimizer.TwoOptSwap(i, k), expected);
+    }
+
+    @Test
+    public void distCalcTest(){
+        Place p0 =  new Place();
+        p0.latitude = "38.452431";
+        p0.longitude = "-107.006570";
+        Place p1 = new Place();
+        p1.latitude = "38.452431";
+        p1.longitude = "-107.380106";
+
+        Place[] trip = {p0, p1};
+        ArrayList<Integer> expected = new ArrayList<Integer>();
+        expected.add(20);
+        assertEquals(optimizer.sumList(trip), expected);
     }
 
 }
